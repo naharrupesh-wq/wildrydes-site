e28ddca2701f77fc28367ea8a21ff28e9f13c5c4
Rupesh Nahar <nahar.rupesh@gmail.com>
2026-06-21 17:05:12 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_jgzIXMBsc', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6mn9iv51i7sajnal05ho3i4mvn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_OTWn1Hi4n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6pu6iv6rl751fejecs3kkufgve', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
